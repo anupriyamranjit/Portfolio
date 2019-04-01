c599f04ebcc0dc31343014b772e75a506218b93b
Anupriyam <anupriyamranjit8@gmail.com>
2019-03-31 22:28:45 -0400

    eff
    fef

--- ProjectOne/WSBS.py
+++ ProjectOne/WSBS.py
@@ -2,9 +2,11 @@
 import bs4 as bs
 import urllib.request
 import pandas as pd
+import numpy as np
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+
 
 
 
@@ -15,7 +17,7 @@ time.sleep(1)
 
 body = browser.find_element_by_tag_name('body')
 
-for i in range(100):
+for i in range(13):
     body.send_keys(Keys.PAGE_DOWN)
     time.sleep(0.1)
 
@@ -23,19 +25,16 @@ tweets = browser.find_elements_by_class_name('tweet-text')
 source = browser.page_source
 soup = bs.BeautifulSoup(source,'lxml')
 
-f= open("ObamaTwitter.txt","w+")
-for m in tweets:
- print(m.text)
- f.write(m.text)
- f.write("\n")
- f.write("\n")
-
-f.write("Tweeted by:"get_handle(soup))
-f.close()
-
-
 def get_handle(BSobject):
     for handle in BSobject.find_all('a', class_='ProfileHeaderCard-screennameLink u-linkComplex js-nav'):
         for at in handle.find_all('b', class_='u-linkComplex-target'):
             print(at.text)
     return(at.text)
+
+
+obamatweet = []
+data_transposed = zip(obamatweet)
+df = pd.DataFrame(data_transposed, columns=["Tweet"])
+print(df.head())
+
+df.to_csv('OBAMATWITTER.csv')
